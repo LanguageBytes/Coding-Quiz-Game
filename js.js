00e67dfa6073c1ca239d759923668db0dbd13266
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-01-10 15:13:50 +0000

    Media Queries

--- js.js
+++ js.js
@@ -4,14 +4,17 @@ document.getElementById("start").addEventListener("click", function(){
     var downloadTimer = setInterval(function function1(){
     document.getElementById("timer").innerHTML = timeleft;
     timeleft -= 1;
+    
+//Questions appear
     var siteTitles = document.querySelectorAll("#Questions")
     for (var i = 0; i < siteTitles.length; i++) {
         siteTitles[i].setAttribute("style", "display:block");
       }
-   
+
+//COUNTDOWN END
     if(timeleft <= 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "Time's up!"
     }
     }, 1000);
     console.log(countdown);
@@ -19,7 +22,7 @@ document.getElementById("start").addEventListener("click", function(){
 
 
 });
-//COUNTDOWN END
+
 
 
 //Questions
