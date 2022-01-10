784004d242ace4de0a8da044d8c7c53b8df35e5b
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-01-10 12:39:13 +0000

    Revealing Questions

--- js.js
+++ js.js
@@ -1,16 +1,26 @@
-//COUNTDOWN
+//COUNTDOWN START
 document.getElementById("start").addEventListener("click", function(){
     var timeleft = 60;
-
     var downloadTimer = setInterval(function function1(){
     document.getElementById("timer").innerHTML = timeleft;
     timeleft -= 1;
+    var siteTitles = document.querySelectorAll("#Questions")
+    for (var i = 0; i < siteTitles.length; i++) {
+        siteTitles[i].setAttribute("style", "display:block");
+      }
+   
     if(timeleft <= 0){
         clearInterval(downloadTimer);
         document.getElementById("countdown").innerHTML = "Time is up!"
     }
     }, 1000);
     console.log(countdown);
+
+
+
 });
 //COUNTDOWN END
+
+
+//Questions
 
